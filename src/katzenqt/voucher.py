61850658c05e1f2bf0f63dc5ef4e24ff796fd048
Jacob Appelbaum <jacob@appelbaum.net>
2026-09-20 19:47:00 +0200

    Keep epoch losses until delivery succeeds
    Fixes: 2f954bba0728

--- src/katzenqt/voucher.py
+++ src/katzenqt/voucher.py
@@ -32,6 +32,7 @@ from .katzen_util import create_task
 from .network import (
     _DAEMON_RPC_TIMEOUT_SECONDS, _SUBSTREAM_NAME_PREFIX, _rpc_racing_connection_life,
     check_for_new, conversation_update_queue, ConnectionLifeInterruptedError,
+    _delivery_racing_connection_life,
 )
 
 logger = logging.getLogger("katzen.voucher")
@@ -198,7 +199,7 @@ async def _publish_box(connection, write_cap: bytes, message_box_index: bytes, p
                 ),
                 backstop_s=_DAEMON_RPC_TIMEOUT_SECONDS,
             )
-            await _rpc_racing_connection_life(
+            await _delivery_racing_connection_life(
                 bacap_uuid=_brief(write_cap), what="start_resending_encrypted_message",
                 rpc_factory=lambda: connection.start_resending_encrypted_message(
                     read_cap=None, write_cap=write_cap, message_box_index=None,
@@ -277,7 +278,7 @@ async def _read_box(
                 ),
                 backstop_s=_DAEMON_RPC_TIMEOUT_SECONDS,
             )
-            resp = await _rpc_racing_connection_life(
+            resp = await _delivery_racing_connection_life(
                 bacap_uuid=_brief(read_cap), what="start_resending_encrypted_message",
                 rpc_factory=lambda: connection.start_resending_encrypted_message(
                     read_cap=read_cap, write_cap=None,
